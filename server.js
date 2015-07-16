5931d1b30f1ba62a066c672f379dbac76a6db5e1
John Mellor <johnme@google.com>
2015-07-16 15:58:21 +0100

    Minimal Express server

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("Ran server.js");
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(req, res) {
+  res.send('Hello world!')
+});
+
+app.listen(app.get('port'), function() {
+    console.log("Node server is running on port", app.get('port'));
+});
