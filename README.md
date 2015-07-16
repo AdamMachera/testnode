e6713e6f09e53635131b63cd09ba5125ea437e8b
John Mellor <johnme@google.com>
2015-07-16 19:37:35 +0100

    Add manifest.json with GCM_SENDER_ID

---
+++ README.md
@@ -0,0 +1,14 @@
+## Setup
+
+```bash
+npm install
+```
+
+## Run
+
+A `GCM_SENDER_ID` environment variable must be passed to the server, for example:
+```bash
+GCM_SENDER_ID=829104499569 npm start
+```
+
+Sender IDs can be obtained [from the Google Developer Console](https://developers.google.com/web/updates/2015/03/push-notificatons-on-the-open-web#make-a-project-on-the-google-developer-console).
--- server.js
+++ server.js
@@ -1,9 +1,19 @@
+var assert = require('assert');
 var express = require('express');
+
+assert(process.env.GCM_SENDER_ID, "GCM_SENDER_ID environment variable not found");
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+
+app.get('/manifest.json', function(req, res) {
+    res.send({
+        'gcm_sender_id': process.env.GCM_SENDER_ID
+    });
+});
 
 app.listen(app.get('port'), function() {
     console.log("Node server is running on port", app.get('port'));
