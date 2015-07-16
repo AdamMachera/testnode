d56f3b90b9869b0c8de3957994b84d1061c357e7
John Mellor <johnme@google.com>
2015-07-16 19:38:35 +0100

    Subscribe with Push API

--- public/index.js
+++ public/index.js
@@ -9,10 +9,42 @@ function error(str) {
 
 if (!navigator.serviceWorker) {
     error("Your browser doesn't support Service Workers.");
+} else if (!window.PushManager) {
+    error("Your browser doesn't support Push.");
+} else if (!window.ServiceWorkerRegistration ||
+           !ServiceWorkerRegistration.prototype.showNotification) {
+    error("Your browser doesn't support Notifications (from Service Workers).");
 } else {
+    $('#subscribe > button').addEventListener('click', onClickSubscribe);
     $('#subscribe > button').disabled = false;
     navigator.serviceWorker.register('sw.js').catch(function(reason) {
-        $('#subscribe > button').disabled = true;
         error("Failed to register Service Worker: " + reason);
+        $('#subscribe > button').disabled = true; // Unrecoverable error.
+    });
+}
+
+function onClickSubscribe() {
+    $('#error').style.display = 'none';
+    $('#subscribe > button').disabled = true;
+    Notification.requestPermission(function(permission) {
+        if (permission == 'granted') {
+            subscribeForPush();
+            return;
+        }
+        error("Notifications permission prompt was " +
+              permission.replace("default", "dismissed") + ".");
+        $('#subscribe > button').disabled = false;
+    });
+}
+
+function subscribeForPush() {
+    navigator.serviceWorker.ready.then(function(swr) {
+        swr.pushManager.subscribe({userVisibleOnly: true}).then(function(ps) {
+            $('#subscribe').style.display = 'none';
+            console.log(JSON.stringify(ps));
+        }, function(reason) {
+            error("Failed to subscribe for Push: " + reason);
+            $('#subscribe > button').disabled = false;
+        });
     });
 }
