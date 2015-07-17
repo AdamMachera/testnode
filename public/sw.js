c14116cc8e2eb33c4207860213aad32a476ea5d0
John Mellor <johnme@google.com>
2015-07-17 21:26:18 +0100

    Show notification when push is received

--- public/sw.js
+++ public/sw.js
@@ -10,4 +10,18 @@ this.addEventListener("activate", function(evt) {
         evt.waitUntil(clients.claim());
 });
 
+this.addEventListener('push', function(evt) {
+    var data = evt.data.json();
+    console.log("SW onpush", data);
+    evt.waitUntil(self.registration.showNotification(data.title, {
+        body: data.body
+    }));
+});
+
+this.addEventListener('notificationclick', function(evt) {
+    console.log("SW notificationclick");
+    evt.notification.close();
+    evt.waitUntil(clients.openWindow("/"));
+});
+
 console.log('Logged from inside SW');
