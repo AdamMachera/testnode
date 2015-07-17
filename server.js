cfade7e2626ec82229337965e464f64a6d8c3d59
John Mellor <johnme@google.com>
2015-07-17 14:49:04 +0100

    Encrypt and send push message

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var assert = require('assert');
 var bodyParser = require('body-parser');
 var express = require('express');
+var request = require('request');
+var util = require('util')
+var webpush = require('node-web-push');
 
+assert(process.env.GCM_API_KEY, "GCM_API_KEY environment variable not found");
 assert(process.env.GCM_SENDER_ID, "GCM_SENDER_ID environment variable not found");
 
 var app = express();
@@ -24,8 +28,40 @@ app.post('/send', jsonParser, function(req, res) {
         if (!req.body.hasOwnProperty(prop))
             return res.status(400).send("Missing " + prop);
     }
-    // TODO: Send a push message to endpoint.
-    res.sendStatus(202);
+    var encrypted = webpush.encrypt({
+        peerPublic: new Buffer(req.body.curve25519dh, 'base64'),
+        plaintext: new Buffer(JSON.stringify({
+            title: req.body.title,
+            body: req.body.body
+        }), 'utf8')
+    });
+    var requestOptions = {
+        url: req.body.endpoint,
+        headers: {
+            'Encryption': util.format('salt="%s"; rs=%s',
+                                      encrypted.salt.toString('base64'),
+                                      encrypted.rs),
+            'Encryption-Key': util.format('dh="%s"',
+                                          encrypted.localPublic.toString(
+                                                  'base64')),
+        },
+        body: encrypted.ciphertext
+    };
+    // Google Cloud Messaging requires proof that the sender is authorized to
+    // use the endpoint (i.e. that they hold the secret API key that corresponds
+    // to the Sender ID given when subscribing on the client).
+    if (/^https:\/\/[-\w]+\.google(apis)?\.com\//.test(req.body.endpoint)) {
+        requestOptions.headers.Authorization = 'key=' + process.env.GCM_API_KEY;
+    }
+    request.post(requestOptions, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.sendStatus(202);
+        } else {
+            // TODO: This is useful for debugging, but should make sure this
+            // doesn't leak sensitive data like the GCM_API_KEY to users.
+            res.status(500).send(util.format(error, response, body));
+        }
+    });
 });
 
 app.listen(app.get('port'), function() {
